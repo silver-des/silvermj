82d4f413d06034f7b862da0bda5246f5edb2cebf
silver-des <ramtin.ataei20@gmail.com>
2016-01-04 14:50:35 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "moderation",
       "plugins",
       "version"},
-    sudo_users = {109722284,119989724,114429017,172887955},
+    sudo_users = {121122782},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
